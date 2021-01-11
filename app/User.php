927506739ee9d17e7a7b47ca3c024a8ed37454cc
Marco Garcia <magrmobile@gmail.com>
2021-01-11 01:57:59 -0600

    Last Changes 11/01/2021 2

--- app/User.php
+++ app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'pivot'
+        'password', 'remember_token', 'pivot', 'email', 'email_verified_at', 'created_at', 'updated_at'
     ];
 
     /**
